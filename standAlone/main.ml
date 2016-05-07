1928eefaf9a7c2abb4bc8926196ae3ba9180e657
Rittyan <ritmo500@gmail.com>
2016-05-07 15:59:35 +0900

    compile test

---
+++ standAlone/main.ml
@@ -0,0 +1,11 @@
+(* main *)
+let main n =
+  let kekka = Fac.f n in
+  ( print_int n;
+    print_string "の階乗は";
+    print_int kekka;
+    print_string " です";
+    print_newline ()
+    )
+
+let _ = main (int_of_string Sys.argv.(1))
